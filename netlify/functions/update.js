b76e9d287616661dde064c80df53413a3d468bcf
Travis Horn <travis@travishorn.com>
2022-02-12 09:59:06 -0600

    Parse event body

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -2,8 +2,9 @@ const axios = require("axios").default;
 
 exports.handler = async (event) => {
   await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-    chat_id: event.body.message.chat.id,
+    chat_id: JSON.parse(event.body).message.chat.id,
     text: "I got your message!",
   });
+
   return { statusCode: 200 };
 };
