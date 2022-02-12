a55da87f4a9bf96deb8b201d94dfcbf699110459
Travis Horn <travis@travishorn.com>
2022-02-12 09:55:21 -0600

    Respond to messages

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,4 +1,9 @@
+const axios = require("axios").default;
+
 exports.handler = async (event) => {
-  console.log("Received an update from Telegram!", event.body);
+  await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+    chat_id: event.body.message.chat.id,
+    text: "I got your message!",
+  });
   return { statusCode: 200 };
 };
