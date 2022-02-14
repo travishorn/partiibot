a1d9a3cd677fabea06e80e9076f45bb21b0d703e
Travis Horn <travis@travishorn.com>
2022-02-14 17:17:36 -0600

    Remove chat id log

--- sendMessage.js
+++ sendMessage.js
@@ -1,7 +1,6 @@
 const axios = require("axios").default;
 
 module.exports = async (chat_id, text) => {
-  console.log(chat_id);
   await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
     chat_id,
     text,
