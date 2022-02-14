02f5b502a4192423d52ce0ab846ec8d48a827786
Travis Horn <travis@travishorn.com>
2022-02-14 17:08:37 -0600

    Log the chat id for testing

--- sendMessage.js
+++ sendMessage.js
@@ -1,6 +1,7 @@
 const axios = require("axios").default;
 
 module.exports = async (chat_id, text) => {
+  console.log(chat_id);
   await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
     chat_id,
     text,
