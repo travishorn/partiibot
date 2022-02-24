b5da3d51fbbe6f6e23917b6f6e4225a397b4a871
Travis Horn <travis@travishorn.com>
2022-02-24 11:38:01 -0600

    Echo and Hashnode Featured

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,12 +1,36 @@
 const sendMessage = require("../../sendMessage");
 const messageParts = require("../../messageParts");
+const hashnode = require("../../hashnode");
 
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
   const { command, botName, extra } = messageParts(message.text);
 
   if (botName === "partiibot" || botName === null) {
-    await sendMessage(message.chat.id, "I got your message!");
+    switch (command) {
+      case "echo":
+        await sendMessage(message.chat.id, extra || "ECHO!");
+        break;
+
+      case "hashnodefeatured":
+        const { storiesFeed } = await hashnode.getFeaturedPosts();
+
+        const reply = `
+${storiesFeed[0].title} by ${storiesFeed[0].author.username}
+
+${storiesFeed[1].title} by ${storiesFeed[1].author.username}
+
+${storiesFeed[2].title} by ${storiesFeed[2].author.username}
+
+https://hashnode.com/featured
+`;
+
+        await sendMessage(message.chat.id, reply);
+        break;
+
+      default:
+        await sendMessage(message.chat.id, "I don't understand that command.");
+    }
   }
 
   return { statusCode: 200 };
